b3547f90f4528b740d15ea66c4a2f378ba146b73
zi <hep.tolentino@gmail.com>
2025-11-25 10:17:57 +0800

    ADDTL FIX: slight adjustment on pages - reduced left margin

--- frontend/src/app/admin-portal/apartment-overview/page.tsx
+++ frontend/src/app/admin-portal/apartment-overview/page.tsx
@@ -11,7 +11,7 @@ const MainContent = () => {
     <div className="flex-1 flex flex-col min-h-screen">
       <header className="bg-white shadow-sm border-b p-2 ">
         <div className="flex items-center gap-4">
-          <h1 className="text-2xl font-bold text-gray-900">Apartment Overview</h1>
+          <h1 className="text-2xl font-bold text-gray-900 ml-3">Apartment Overview</h1>
         </div>
       </header>
 
